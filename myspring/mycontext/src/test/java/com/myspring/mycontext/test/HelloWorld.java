7d8ed2aa45f09c2eaa3ffa253dc92ca7dac42fd0
zhy12306 <18629439541@163.com>
2020-01-27 14:42:52 +0800

    添加通过读取 xml 配置，实现 bean 属性注入

--- myspring/mycontext/src/test/java/com/myspring/mycontext/test/HelloWorld.java
+++ myspring/mycontext/src/test/java/com/myspring/mycontext/test/HelloWorld.java
@@ -8,12 +8,17 @@ package com.myspring.mycontext.test;
  */
 public class HelloWorld {
     private String whatUWannaSay;
+    private  String reason;
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
 
     public void setWhatUWannaSay(String whatUWannaSay) {
         this.whatUWannaSay = whatUWannaSay;
     }
 
     public void sayHello() {
-        System.out.println("Hello world!\r\n"+whatUWannaSay+"\r\nI'm waiting for you!!");
+        System.out.println("Hello world!\r\n"+whatUWannaSay+"\r\nI'm waiting for you!!\r\n"+reason);
     }
 }
